4cd51ca46545a4de96c5dcf536d5fb15f684d444
Pedro <paasxx@gmail.com>
2025-02-28 20:54:01 -0300

    Home criada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { CryptoListComponent } from './pages/crypto-list/crypto-list.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
 
     {
         path: '',
+        pathMatch: 'full',
+        component: HomeComponent
+    },
+    {
+        path: 'data',
         pathMatch: 'full',
         component: CryptoListComponent
     },
